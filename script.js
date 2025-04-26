561335f5215e0be7d534eb29acbab62c795e68f6
rafael-gar <rafaelmg2513@gmail.com>
2025-04-26 13:34:15 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var data = [
+{ nombre: 'perfume mujer 1', categoria: 'mujer'},
+{ nombre: 'perfume hombre 1', categoria: 'hombre'},
+{ nombre: 'perfume unisex 1', categoria: 'unisex'},
+            ];
+
+
